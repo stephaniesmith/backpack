e6ad2d7db27b2997c32a30dcbf93aa53110db2f7
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-12-12 14:49:09 -0800

    fix my test

--- client/src/actions/gear.test.js
+++ client/src/actions/gear.test.js
@@ -5,7 +5,6 @@ jest.mock('../services/gear.js');
 describe('artists actions', () => {
   it('has a promise as its payload', () => {
     const action = fetchGear();
-    // expect(typeof action.payload.then).toEqual('function');
     expect(action.type).toEqual(FETCH_GEAR);
   });
 });
