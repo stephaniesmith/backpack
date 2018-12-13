ac21908b063e27db55e92e79af9aaee02f02c45b
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-12-13 13:08:02 -0800

    simple styles to header

--- client/src/components/app/Header.js
+++ client/src/components/app/Header.js
@@ -4,7 +4,7 @@ import { rootLinks } from '../../routes';
 export default function Header() {
   return (
     <header>
-      <nav>
+      <nav style={{ display: 'flex', justifyContent: 'space-between', width: 200 }}>
         {rootLinks()}
       </nav>
     </header>
