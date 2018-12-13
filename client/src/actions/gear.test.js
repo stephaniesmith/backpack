e350843fb495a5b755948324ad4463971791904f
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-12-12 19:12:08 -0800

    typo

--- client/src/actions/gear.test.js
+++ client/src/actions/gear.test.js
@@ -2,7 +2,7 @@ import { fetchGear, FETCH_GEAR } from './gear';
 
 jest.mock('../services/gear.js');
 
-describe('artists actions', () => {
+describe('gear actions', () => {
   it('has a promise as its payload', () => {
     const action = fetchGear();
     expect(action.type).toEqual(FETCH_GEAR);
