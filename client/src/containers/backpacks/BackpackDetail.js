07bf4d35a6ed0f74a9f9b0c8cb280a144bac9557
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-12-13 13:22:21 -0800

    detail container

---
+++ client/src/containers/backpacks/BackpackDetail.js
@@ -0,0 +1,19 @@
+import { connect } from 'react-redux';
+// import { fetchBackpack } from '../../actions/backpacks';
+// import { getBackpack } from '../../selector/backpacks';
+import BackpackDetail from '../../components/backpacks/BackpackDetail';
+import { withFetch } from '../../components/withFetch';
+
+const mapStateToProps = state => ({
+  // detail: getBackpack(state)
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetch: () => console.log('HELLOOOW!!')
+  // fetch: () => dispatch(fetchBackpack())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withFetch(BackpackDetail));
