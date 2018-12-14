2bd5587db4b513c9a427f3f43f6f99582520ebb3
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-12-14 09:39:35 -0800

    createGear action

--- client/src/actions/gear.js
+++ client/src/actions/gear.js
@@ -1,6 +1,7 @@
-import { getGear } from '../services/gear';
+import { getGear, postGear } from '../services/gear';
 
 export const FETCH_GEAR = 'FETCH_GEAR';
+export const CREATE_GEAR = 'CREATE_GEAR';
 export const FETCH_GEAR_LOADING = 'FETCH_GEAR_LOADING';
 export const FETCH_GEAR_DONE = 'FETCH_GEAR_DONE';
 
@@ -9,4 +10,11 @@ export const fetchGear = () => ({
   loadStart: FETCH_GEAR_LOADING,
   loadEnd: FETCH_GEAR_DONE,
   payload: getGear()
+});
+
+export const createGear = gear => ({
+  type: CREATE_GEAR,
+  loadStart: FETCH_GEAR_LOADING,
+  loadEnd: FETCH_GEAR_DONE,
+  payload: postGear(gear)
 });
