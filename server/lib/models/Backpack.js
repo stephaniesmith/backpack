a7880ce9adbd612faaf01c369da82a745844227d
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-12-13 19:12:47 -0800

    refactoring gearWeight agg

--- server/lib/models/Backpack.js
+++ server/lib/models/Backpack.js
@@ -43,7 +43,8 @@ schema.statics = {
             .lean();
     },
     gearWeight(id) {
-        return this.aggregate(weighGear(id));
+        return this.aggregate(weighGear(id))
+            .then(results => results[0].weight);
     }
 };
 
